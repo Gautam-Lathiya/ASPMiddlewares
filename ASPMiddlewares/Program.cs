c70b0ac32a93d37eee383daf781221b2b7701aac
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-04-08 12:08:20 +0530

    Correct use of Map middleware

--- ASPMiddlewares/Program.cs
+++ ASPMiddlewares/Program.cs
@@ -10,9 +10,14 @@ app.Use(async (context, next) =>
     await context.Response.WriteAsync("Hello from the first middleware after next!\n");
 });
 
-app.Map("/", async (context) =>
+app.Map("/xyz", xyz =>
 {
-    await context.Response.WriteAsync("Hello from the Gautam middleware!\n");
+    xyz.Use(async (context,next) =>
+    {
+        await context.Response.WriteAsync("Hello from the Gautam middleware!\n");
+        await next();
+    });
+    xyz.Run(async c => await c.Response.WriteAsync("Last Run"));
 });
 
 app.Run(async (context) =>
